16316ca514a539ea8e564849fe4cef6e81cb00df
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2025-02-17 15:47:43 +0300

    Get tasks joined with employee

--- app/tasks/schemas.py
+++ app/tasks/schemas.py
@@ -9,6 +9,7 @@ class STask(BaseModel):
     name: str = Field(..., description='Name of the task')
     parent_task: Optional[int] = Field(None, description='ID of the parent task')
     employee_id: Optional[int] = Field(None, description='ID of the assignee')
+    employee: Optional[str] = Field(None, description='Full name of the assignee')
     deadline: Optional[date] = Field(None, description='Expiration date of the task')
     status: Optional[str] = Field(None, description='Status of the task')
     created_at: datetime = Field(..., description='Timestamp of the creation')
