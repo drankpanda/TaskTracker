3ee5b59a79096629e9d2d00dbffc0ea9c20d3da3
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2025-02-23 12:55:32 +0300

    Add GET favicon

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ from typing import Optional
 from random import random
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 from app.tasks.router import router as tasks_router
 from app.employees.router import router as employees_router
 
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 
 @app.get('/')
@@ -16,6 +17,14 @@ def test_welcome_script(message: Optional[str] = 'Greetings!'):
         'random_number': int(random()*100),
         'message': message,
     }
+
+
+@app.get('/favicon.ico')
+def get_favicon():
+    file_name: str = 'favicon.png'
+    file_path: str = f'app\\static\\images\\{file_name}'
+    return FileResponse(path=file_path, headers={'Content-Disposition': f'attachment; filename={file_name}'})
+
 
 app.include_router(tasks_router)
 app.include_router(employees_router)
