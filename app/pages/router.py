1b8689670f3c65f96625ac5ccf331ed16a9ff29b
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2025-03-08 01:42:43 +0300

    Structure jinja templates

--- app/pages/router.py
+++ app/pages/router.py
@@ -5,11 +5,11 @@ from app.employees.router import get_all_employees
 from app.tasks.router import get_all_tasks
 
 router = APIRouter(prefix='/view', tags=['Frontend'])
-templates = Jinja2Templates(directory='app/templates')
+templates = Jinja2Templates(directory='app/templates', trim_blocks=True, lstrip_blocks=True)
 
 
 @router.get('/tasks')
 async def get_students_html(request: Request, tasks=Depends(get_all_tasks), employees=Depends(get_all_employees)):
-    return templates.TemplateResponse(name='tasks.html', context={'request': request,
-                                                                  'tasks': tasks,
-                                                                  'employees': employees})
+    return templates.TemplateResponse(name='tasks/tasks.html', context={'request': request,
+                                                                        'tasks': tasks,
+                                                                        'employees': employees})
